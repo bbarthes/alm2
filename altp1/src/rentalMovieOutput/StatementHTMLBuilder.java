50369b719dbb06392e2a23ac7b50a575258a3c45
Benoit Barthes <bbarthes@emi.u-bordeaux1.fr>
2014-10-01 15:28:41 +0200

    fin implementation de la vertion html

--- altp1/src/rentalMovieOutput/StatementHTMLBuilder.java
+++ altp1/src/rentalMovieOutput/StatementHTMLBuilder.java
@@ -7,37 +7,61 @@ public class StatementHTMLBuilder implements StatementBuilder{
 	private String name;
 	private double price;
 	private int renterPoint;
+	private Vector<Tuple> movies;
 	
 	
+	
+	
+	public StatementHTMLBuilder() {
+		movies = new Vector<>();
+	}
+
 	@Override
 	public void setTitle(String name) {
 		
-		
+		this.name = name;
 		
 	}
 
 	@Override
 	public void addMovie(String name, double price) {
-		// TODO Auto-generated method stub
+		movies.add(new Tuple(name, price));
 		
 	}
 
 	@Override
 	public void setTotalPrice(double price) {
-		// TODO Auto-generated method stub
+		this.price= price;
 		
 	}
 
 	@Override
 	public void setTotalRenterPoint(int points) {
-		// TODO Auto-generated method stub
-		
+		this.renterPoint = points;
 	}
-
-	@Override
-	public String getStatement() {
-		// TODO Auto-generated method stub
-		return null;
+	
+	public String getStatement(){
+		StringBuffer html = new StringBuffer();
+		html.append("<article> <head> Rental Record for ");
+		html.append(this.name);
+		html.append("</head>");
+		for(Tuple t : movies)
+		{
+			html.append("<section>");
+			html.append(t.getName());
+			html.append("&nbsp");
+			html.append(t.getPrice());
+			html.append("</section>");
+		}
+		html.append("<footer>");
+		html.append("Amount owned is ");
+		html.append(this.price);
+		html.append("</br>You earned ");
+		html.append(this.renterPoint);
+		html.append(" frequent renter points.");
+		html.append("</footer>");
+		html.append("</article>");
+		return html.toString();
 	}
 
 }
