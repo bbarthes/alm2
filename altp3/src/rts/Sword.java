576a3862f99fb6d8fe210b16988c63ca5eafbe18
Benoit Barthes <bbarthes@emi.u-bordeaux1.fr>
2014-10-22 12:26:46 +0200

    supression doc

--- altp3/src/rts/Sword.java
+++ altp3/src/rts/Sword.java
@@ -13,29 +13,25 @@ public class Sword extends Equipment {
 	 * @param next
 	 * @param health
 	 */
-	public Sword(SoldierComponent next, int health) {
-		super(next, health);
+	public Sword(SoldierComponent deco, int health) {
+		super(deco, health);
+	}
+	public Sword(SoldierComponent deco) {
+		super(deco, 100);
 	}
 
-
-	/* (non-Javadoc)
-	 * @see rts.SoldierComponent#strike()
-	 */
 	@Override
 	public int strike() {
 		return super.strike()+super.strength;
 	}
 
-	/* (non-Javadoc)
-	 * @see rts.SoldierComponent#parry(int)
-	 */
 	@Override
 	public void parry(int strengthEnnemy) {
 		if(super.rand.nextInt() % 10 == 0)
 		{
 			strengthEnnemy = 0;
 		}
-		
+
 		super.parry(strengthEnnemy);
 
 	}
