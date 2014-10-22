5f2202f8b8e8fbf0b9a31361c112e9daa2a95f2c
Benoit Barthes <bbarthes@emi.u-bordeaux1.fr>
2014-10-22 12:25:44 +0200

    programme de teste

---
+++ altp3/src/prog/Programme.java
@@ -0,0 +1,62 @@
+package prog;
+
+import rts.Horseman;
+import rts.Infantryman;
+import rts.SoldierComponent;
+import rts.Sword;
+
+public class Programme {
+
+	private Tuple<String ,SoldierComponent> soldatAtk, soldatDef ;
+	
+	public Programme() {
+		soldatAtk = new Tuple<String, SoldierComponent>("gérard", new Horseman());
+		soldatDef = new Tuple<String, SoldierComponent>("bernard",new Sword(new Infantryman()));
+	}
+	
+	
+	void invertAtkDeff()
+	{
+		Tuple<String ,SoldierComponent> swap;
+		swap = this.soldatAtk;
+		this.soldatAtk = this.soldatDef;
+		this.soldatDef = swap;
+		
+	}
+	
+	int bataille()
+	{
+		int strength = this.soldatAtk.getValue().strike();
+		this.soldatDef.getValue().parry(strength);
+		
+		return strength;
+		
+	}
+	
+	void raportBataille()
+	{
+		int bataille = this.bataille();
+		int healthPoints = this.soldatDef.getValue().getHealthPoints();
+		System.out.println("le " + this.soldatAtk.getName() + " frappe de " + bataille + " le " + this.soldatDef.getName() + " qui a " + healthPoints +"point de vie");
+	}
+	
+	boolean endBataille()
+	{
+		return this.soldatAtk.getValue().getHealthPoints() <= 0 || this.soldatDef.getValue().getHealthPoints() <= 0;
+	}
+	
+	int main (){
+
+		Programme p = new Programme();
+
+		while (endBataille()) {
+			
+			p.bataille();
+			p.invertAtkDeff();
+
+		}
+
+		return 0;
+	}
+}
+
