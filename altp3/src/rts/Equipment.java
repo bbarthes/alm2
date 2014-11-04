745440f4cdd927b1da62fe4aa7630ec03f86dd00
benoît barthés <benoit.barthes@etu.u-bordeaux1.fr>
2014-11-04 01:11:52 +0100

    exempel idée limitation arme caca

--- altp3/src/rts/Equipment.java
+++ altp3/src/rts/Equipment.java
@@ -5,12 +5,32 @@ import java.util.Random;
 public abstract class Equipment extends Decorator{
 
 	int strength;
+	int numberEquipement;
 	Random rand;
 
-	public Equipment(SoldierComponent deco, int health) {
+	public Equipment(SoldierComponent deco, int health) throws Exception {
 		super(deco);
+		this.numberEquipement = 0;
+		Class<?> classe = deco.getClass();
+
+		for(Class<?> superClass = classe.getSuperclass();superClass != null; superClass = classe.getSuperclass())
+		{
+			if(superClass.getName() == "Equipment")
+			{
+				Equipment eq = (Equipment)deco;
+				this.numberEquipement = ++eq.numberEquipement;
+			}
+		}
+		if(this.numberEquipement >= 3)
+		{
+			throw new Exception("trops d'équiment dans les mains");
+		}
 		this.strength = health;
 		this.rand = new Random();
+	}
+
+	protected int getNumberEquipement() {
+		return this.numberEquipement;
 	}
 
 	protected int reduceDamage(int reduce , int damage){
