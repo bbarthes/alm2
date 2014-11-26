a289bf3d8da0b37113bd5f9f3f4a0fb7ee77f5f8
barthès benoît <benoit.barthes@etu.u-bordeaux.fr>
2014-11-26 11:23:25 +0100

    corection orthographe

--- altp4/src/rts/visitor/VisitorArmyPrint.java
+++ altp4/src/rts/visitor/VisitorArmyPrint.java
@@ -8,19 +8,19 @@ public class VisitorArmyPrint implements IVisitorArmy {
 
 	@Override
 	public void visit(Army army) {
-		System.out.println("Visite d'une Armée");
+		System.out.println("Visite d'une armée");
 
 	}
 
 	@Override
 	public void visit(HorsemanFacade horsmam) {
-		System.out.println("\t visite d'un horsman");
+		System.out.println("\t visite d'un horseman");
 
 	}
 
 	@Override
 	public void visit(InfantrymanFacade infantryman) {
-		System.out.println("\t visite d'un infranryman");
+		System.out.println("\t visite d'un infantryman");
 
 	}
 
