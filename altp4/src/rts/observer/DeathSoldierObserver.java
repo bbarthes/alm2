80339db39358821aa5da2781f470c1e98d73dd72
Anne-laure Mesure <bluewolf33@hotmail.fr>
2014-12-03 10:55:53 +0100

    Ajouts classes pour observer

--- altp4/src/rts/observer/DeathSoldierObserver.java
+++ altp4/src/rts/observer/DeathSoldierObserver.java
@@ -1,16 +1,14 @@
 package rts.observer;
 
-import java.util.Observable;
-import java.util.Observer;
 
-public class DeathSoldierObserver implements Observer{
+public class DeathSoldierObserver implements MyObserver{
 	
 	private int nbDeath = 0;
 	
 	
 
 	@Override
-	public void update(Observable arg0, Object arg1) {
+	public void update(MyObservable arg0, Object arg1) {
 		++nbDeath;
 		System.out.println(nbDeath + "soldiers have been killed, RIP.");
 		
--- altp4/src/rts/observer/MyObserver.java
+++ altp4/src/rts/observer/MyObserver.java
@@ -2,4 +2,6 @@ package rts.observer;
 
 public interface MyObserver {
 
+	void update(MyObservable arg0, Object arg1);
+
 }
